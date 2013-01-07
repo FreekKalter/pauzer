8fae6456b6d9d93dda78e3bb7e1faca0f62c9de9
Freek Kalter <freek@kalteronline.org>
2013-01-07 22:29:23 +0100

    call Get in goroutine for fast website response, use 303 redirect

--- pauzer.go
+++ pauzer.go
@@ -9,8 +9,8 @@ import (
 	"net/http"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
-    "strings"
 )
 
 // ignore invalid certificates (todo: make it accecpt a valid cert)
@@ -38,58 +38,56 @@ func HomeHandler(
 
 	timer_expire := timer_set_at.Add(timer_duration)
 	template_data := TemplateData{}
-    if timer_duration == -1 {
-        template_data.Error = "invalid time given"
-    }
+	if timer_duration == -1 {
+		template_data.Error = "invalid time given"
+	}
 
 	if time.Now().After(timer_set_at.Add(timer_duration)) {
 		err = tmpl.ExecuteTemplate(w, "root", template_data)
 	} else {
-        template_data.Time = timer_expire.Format(time.ANSIC)
+		template_data.Time = timer_expire.Format(time.ANSIC)
 		err = tmpl.ExecuteTemplate(w, "root", template_data)
 	}
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
 
 func ResumeHandler(w http.ResponseWriter, r *http.Request) {
 	resume_url := "https://localhost:9100/sabnzbd/api?mode=resume&apikey=d2ef95d20181d30d884321fb9cb68cbe"
 	timer_set_at = time.Now()
 	timer_duration = 0
-	resp, err := client.Get(resume_url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	_, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	http.Redirect(w, r, "/", 302)
+    call_sabnzbd(resume_url)
+	http.Redirect(w, r, "/", 303)
 }
 
 func PauseHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm() // get post data for extraction in r.FormValue
 	time_regex := regexp.MustCompile("^[0-9]{1,2}$")
-	if ! time_regex.MatchString(strings.TrimSpace(r.FormValue("time"))) {
-		timer_duration=-1
-        fmt.Println("invalid data")
-	}else{
-        timer_value, _ := strconv.ParseInt(r.FormValue("time"), 0, 32)
-        timer_duration = time.Minute * time.Duration(timer_value)
-        timer_set_at = time.Now()
+	if !time_regex.MatchString(strings.TrimSpace(r.FormValue("time"))) {
+		timer_duration = -1
+		fmt.Println("invalid data")
+	} else {
+		timer_value, _ := strconv.ParseInt(r.FormValue("time"), 0, 32)
+		timer_duration = time.Minute * time.Duration(timer_value)
+		timer_set_at = time.Now()
 
-        pause_url := fmt.Sprintf("https://localhost:9100/sabnzbd/api?mode=config&name=set_pause&value=%v&apikey=d2ef95d20181d30d884321fb9cb68cbe", timer_value)
+		pause_url := fmt.Sprintf("https://localhost:9100/sabnzbd/api?mode=config&name=set_pause&value=%v&apikey=d2ef95d20181d30d884321fb9cb68cbe", timer_value)
+		go call_sabnzbd(pause_url)
+	}
+	http.Redirect(w, r, "/", 303)
+}
 
-        resp, err := client.Get(pause_url)
-        if err != nil {
-            panic(err)
-        }
-        defer resp.Body.Close()
-        _, err = ioutil.ReadAll(resp.Body)
-    }
-    http.Redirect(w, r, "/", 302)
+func call_sabnzbd(url string) {
+	resp, err := client.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
