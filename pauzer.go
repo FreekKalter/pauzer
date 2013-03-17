80771f0f3dc0de5e5c1d0c9f44055befc9a67efa
Freek Kalter <freek@kalteronline.org>
2013-03-17 22:13:03 +0100

    added License

--- pauzer.go
+++ pauzer.go
@@ -1,3 +1,8 @@
+// Copyright 2013 (c) Freek Kalter. All rights reserved.
+// Use of this source code is governed by the "Revised BSD License"
+// that can be found in the LICENSE file.
+
+// pauzer is a website to pause a local sabnzb instance without exposing the whole interface.
 package main
 
 import (
